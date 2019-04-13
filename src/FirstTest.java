8200f1fc59a2bf16b9bdcb1f2ac25d482f451793
Anton <belous.nsk@gmail.com>
2019-04-13 14:08:39 +0300

    Lesson_4.1 (mac)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,70 +1,41 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
+public class FirstTest extends CoreTestCase {
 
-    private AppiumDriver driver;
+    @Test
+    public void testSearch() {
 
-    @Before
-    public void setUp() throws Exception {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        WebElement search_element = waitForElementPresent(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search input",
+                5
+        );
 
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app",
-                "/Users/anton/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        String search_text = search_element.getAttribute("text");
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        Assert.assertEquals(
+                "We see unexpected text",
+                "Search…",
+                search_text
+        );
     }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
-
-//    @Test
-//    public void firstTest() {
-//
-//        waitForElementAndClick(
-//                By.id("org.wikipedia:id/search_container"),
-//                "Cannot find 'Search Wikipedia' input",
-//                5
-//        );
-//
-//        WebElement search_element = waitForElementPresent(
-//                By.id("org.wikipedia:id/search_src_text"),
-//                "Cannot find search input",
-//                5
-//        );
-//
-//        String search_text = search_element.getAttribute("text");
-//
-//        Assert.assertEquals(
-//                "We see unexpected text",
-//                "Search…",
-//                search_text
-//        );
-//    }
 
     @Test
     public void testSwipeArticle() {
@@ -101,7 +72,7 @@ public class FirstTest {
     }
 
     @Test
-    public void saveFirstArticleToMyList() {
+    public void testSaveFirstArticleToMyList() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "Cannot find 'Search Wikipedia' input",
